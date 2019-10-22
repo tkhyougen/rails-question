d17e7a1f6eb8e904ca9764a7744cbd55c1ad106f
tomohiko <illgalsubstance@yahoo.co.jp>
2019-10-22 03:58:14 +0000

    rails

---
+++ sample/app/controllers/blogs_controller.rb
@@ -0,0 +1,37 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs=Blog.all
+    binding.pry
+  end
+
+  def stocks
+  end
+
+  def new
+    @blog=Blog.new
+  end
+
+  def create
+    @blog=Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice:"ブログを作成しました"
+    else
+     render:new
+    end
+  end
+
+  def show
+    @blog=Blog.find(params[:id])
+  end
+
+  def index
+    @blogs=Blog.all
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+
+end
